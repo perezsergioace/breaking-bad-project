1458781d1e958fd5f57ce01c69b7a97826f22d4d
Sergio Perez <perez.sergio.ace@gmail.com>
2020-05-25 21:02:10 -0400

    successfully getting data back from the api

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('hello')
+const getAllCharacters = async () => {
+	const base = 'https://www.breakingbadapi.com/api/characters'
+
+	try {
+		const response = await fetch(base)
+		const data = await response.json()
+		console.log(data)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+getAllCharacters()
