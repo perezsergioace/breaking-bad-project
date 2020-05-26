e8a8e263742cbc55b61791f7ba057a5dd402955b
Sergio Perez <perez.sergio.ace@gmail.com>
2020-05-25 21:21:18 -0400

    successfully printing each individual character's name to the screen. will print the other appropriate information and style the page accordingly

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const charactersList = document.getElementById('charactersList')
+let bbCharacters = []
+
 const getAllCharacters = async () => {
 	const base = 'https://www.breakingbadapi.com/api/characters'
 
@@ -6,12 +9,26 @@ const getAllCharacters = async () => {
 		if (!response.ok) {
 			throw new Error(response.status)
 		}
-		const data = await response.json()
-		console.log(data)
+		bbCharacters = await response.json()
+		displayCharacters(bbCharacters)
 	} catch (error) {
 		console.log('Something is wrong here!')
 		console.log(error)
 	}
 }
 
-// getAllCharacters()
+const displayCharacters = (characters) => {
+	const htmlString = characters
+		.map((character) => {
+			return `
+            <li class="character">
+                <h2>${character.name}</h2>
+            </li>
+            `
+		})
+		.join('')
+	console.log(characters)
+	charactersList.innerHTML = htmlString
+}
+
+getAllCharacters()
